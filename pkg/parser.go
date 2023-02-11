326875b494ce3274a5c7d6d7f34d8128351267d6
rakhmeto <abrambest2008@yandex.ru>
2023-02-11 13:41:46 +0600

    2023.02.11

--- pkg/parser.go
+++ pkg/parser.go
@@ -15,12 +15,12 @@ func checkErr(err error) {
 }
 
 type StructArtist struct {
-	Id           int    `json:"id"`
-	Image        string `json:"image"`
-	Name         string //`json:"name"`
-	Members      string `json:"members"`
-	CreationDate int    `json:"creationDate"`
-	FirstAlbum   string
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
 }
 
 var Artist []StructArtist
